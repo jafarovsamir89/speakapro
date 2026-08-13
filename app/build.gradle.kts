639776eb815a4a7f04a83d6458903bb72dd9a396
jafarovsamir89 <jafarovsamir@gmail.com>
2026-08-14 02:16:21 +0400

    Use stable Android API 36 for MVP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "az.simplesoft.speakapro"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "az.simplesoft.speakapro"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
     }
