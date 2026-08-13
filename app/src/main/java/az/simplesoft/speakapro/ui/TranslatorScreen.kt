89dc9ef8daed2900429fe4790628bb41ea20a0fb
jafarovsamir89 <jafarovsamir@gmail.com>
2026-08-14 02:35:04 +0400

    Fix Compose layout imports

--- app/src/main/java/az/simplesoft/speakapro/ui/TranslatorScreen.kt
+++ app/src/main/java/az/simplesoft/speakapro/ui/TranslatorScreen.kt
@@ -3,16 +3,7 @@ package az.simplesoft.speakapro.ui
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DropdownMenu
